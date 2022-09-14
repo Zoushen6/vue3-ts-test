6a322f21368300c80ac175fad3f00d4b0271f943
祖士洋 <zushiyang@idataway.com>
2022-09-14 11:18:25 +0800

    🎨 fix: to has
    hash

--- src/router/router.ts
+++ src/router/router.ts
@@ -58,7 +58,7 @@ const routes: Array<RouteRecordRaw> = [
  */
 const router = createRouter({
     //createWebHashHistory是hash模式，它在 SEO 中有不好的影响
-    history: createWebHistory('/'),  // 设置为history模式，就是路径里面没有#,  createWebHashHistory 是默认的，括号里面的就是基础路径，可以理解为项目名称，就是请求路径的基础url
+    history: createWebHashHistory('/'),  // 设置为history模式，就是路径里面没有#,  createWebHashHistory 是默认的，括号里面的就是基础路径，可以理解为项目名称，就是请求路径的基础url
     routes
 })
 
